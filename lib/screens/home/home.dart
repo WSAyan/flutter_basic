c7d178ce3a1429f414d62ac80a26470027778896
wahid.ayan <wahid.sadique@sslwireless.com>
2021-03-01 11:47:56 +0600

    add push notifications

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_basic/screens/home/drawer.dart';
@@ -11,11 +12,62 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeState extends State<HomeScreen> {
+  String messageTitle = "Empty";
+  String notificationAlert = "alert";
+
+  FirebaseMessaging firebaseMessaging = FirebaseMessaging();
+
+  @override
+  void initState() {
+    super.initState();
+
+    firebaseMessaging.configure(
+      onMessage: (message) async {
+        setState(() {
+          messageTitle = message["notification"]["title"];
+          notificationAlert = "New Notification Alert";
+        });
+      },
+      onResume: (message) async {
+        setState(() {
+          messageTitle = message["data"]["title"];
+          notificationAlert = "Application opened from Notification";
+        });
+      },
+    );
+
+    firebaseMessaging.requestNotificationPermissions();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(AppStrings.HOME_SCREEN_TITLE)),
-      body: Center(child: Text('My Page!')),
+      appBar: AppBar(
+        title: Text(AppStrings.HOME_SCREEN_TITLE),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.notifications,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              notificationAlert,
+            ),
+            Text(
+              messageTitle,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
       drawer: SideNavDrawer(),
     );
   }
